a7b986b8a4049d30f981f129017e1bdffd0f021c
Юлия <108525379+yul0173@users.noreply.github.com>
2022-07-17 11:45:49 +0300

    Задача 2

---
+++ Zadasha_1/Zadasha_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число ");
+int num = int.Parse(Console.ReadLine());
+if (num > 99)
+{
+    Console.WriteLine("Третья цифра = " + num % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
