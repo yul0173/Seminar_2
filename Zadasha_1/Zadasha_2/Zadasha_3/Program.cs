1672655c7f542739e642e7602dae960ccc180441
Юлия <108525379+yul0173@users.noreply.github.com>
2022-07-17 11:47:46 +0300

    Задача 3

---
+++ Zadasha_1/Zadasha_2/Zadasha_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1 до 7 ");
+int num = int.Parse(Console.ReadLine());
+if (num > 5)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
+
